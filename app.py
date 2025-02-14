6806adbcf405a7446c4b46c5c5b90b8cd62495ad
faiqilmupadi <muhammadfaiqas88@gmail.com>
2025-02-14 09:03:29 +0700

    app yg bagus

--- app.py
+++ app.py
@@ -3,26 +3,35 @@ import sys
 sys.dont_write_bytecode = True
 load_dotenv()
 import clickhouse_connect
+import os
 
 from flask import Flask, request
 from flask_restful import Resource, Api
 from flask_cors import CORS
 from backend.dashboard import Dashboard
 from backend.request import Request
-from backend.chatbotapp import ProjectChatService
-from backend.chatbotapp import ChatHistoryService
-from backend.databaseopenlit import client
+from backend.admin.chatbotapp import ProjectChatService
+from backend.admin.chatbotapp import ChatHistoryService
+from backend.database.databaseopenlit import client
 from backend.exception import Exception
-from backend.apiKeys import apiKeys
-from backend.appcatalogue import AppCatalogue
+from backend.admin.apiKeys import apiKeys
+from backend.admin.appcatalogue import AppCatalogue
 from backend.login import AuthApp  
-from backend.pricing import PricingAPI
-import os
+from backend.admin.pricing import PricingAPI
+from backend.admin.vault import vault
 
 app = Flask(__name__)
 
 debug = True
-CORS(app, resources={r"*": {"origins": "*"}})
+CORS(app, 
+     resources={r"*": {
+         "origins": ["http://localhost:3000", "http://127.0.0.1:3000"],  # Tambahkan kedua format URL
+         "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+         "allow_headers": ["Content-Type", "Authorization", "Accept"],
+         "supports_credentials": True,
+         "allow_credentials": True  # Tambahkan ini
+     }},
+     expose_headers=["Content-Type", "Authorization"])
 api = Api(app)
 
 client =  clickhouse_connect.get_client(host='openlit.my.id', port='8123', database="openlit", username='default',password='OPENLIT',
@@ -36,6 +45,7 @@ api.add_resource(ChatHistoryService, '/api/chathistory/<string:unique_id_chat>')
 api.add_resource(Exception, '/api/tracesExceptions/', '/api/tracesRequest/<string:appName>')
 api.add_resource(AppCatalogue, "/appcatalogue")
 app.register_blueprint(apiKeys, url_prefix='/apiKeys')
+app.register_blueprint(vault, url_prefix='/vault')
 AuthApp(app)
 PricingAPI(app)
 
