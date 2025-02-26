f0d40e5002ce3bcdb1836ba9a4c02815d7d6d397
AhlisDinalBahtiar <ahlzdinal.1005@gmail.com>
2025-02-26 22:04:25 +0700

    solve import alert error

--- components/Vault/AddKeyVault.tsx
+++ components/Vault/AddKeyVault.tsx
@@ -9,6 +9,8 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { VaultFormData } from "@/types/vault";
+import { Alert, AlertTitle, AlertDescription } from "@/components/ui/alert";
+import { AlertTriangle } from "lucide-react";
 
 interface AddKeyModalProps {
   isOpen: boolean;
